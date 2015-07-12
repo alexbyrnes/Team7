3d7db58f8f385336b587f71a96a0a59c35203b09
Melissa Steffan <msteffan2012@gmail.com>
2015-07-12 10:43:32 -0400

    completed jquery funciton to load next story from API data on each click - woohoo

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,46 @@
 //global variable
 var storyCounter = 0;
 
-// jQuery function to load articles based on what's clicked
-$("#spaz").on("click", getJSON);
-$("#microwaver").on("click", getJSON);
-$("#commuter").on("click", getJSON);
-$("#sunday").on("click", getJSON);
-
 // command for clicking on the left & right divs
 $("#trash").click(trashArticle);
 $("#nextStory").click(getJSON)
 
-// bring on the json
-// jquery function to pull in article information to article page
-// .parseJSON?
+// bring on the json. jquery-->ajax function to pull in article information to article page
 function getJSON(){
-//needs to empty out existing containers and revert to empty state --> refresh the page
-    location.reload();
-// get JSON data for next story based on value of storyCounter. storyCounter = 0 is the [0] array in the JSON object; the storyCounter grows by 1 each time it is clicked, returning the next value in the object each time.
-    $.getJSON('http://10.90.12.47:5000/articles', function(data) {
-        // depending on what story you are reading, when you click next, it will return the next item
-        // need to pull in article image
-        $('.articleHeadline').html(data.headline[storyCounter]);
-        $('.byline').html(data.author[storyCounter]);
-        $('.articleText').append('<p>' + data.body[storyCounter] + '</p>');
-    // add one to storyCounter
-        storyCounter++;
-
-   });
+   $.ajax({
+       url: "http://10.90.11.128:5000/articles",
+        //force to handle it as text
+        dataType: "text",
+        success: function(data) {
+            // store the parsed json as a variable
+            json = $.parseJSON(data);
+            // // get JSON data for next story based on value of storyCounter. storyCounter = 0 is the [0] array in the JSON object; the storyCounter grows by 1 each time it is clicked, returning the next value in the object each time.
+            $('.byline').html("By " + json[storyCounter].author);
+            $('.articleHeadline').html(json[storyCounter].headline);
+            $('.articleText').html(json[storyCounter].copy);
+            }
+        });
+    storyCounter++;
 }
 
-function getBullets() {
-//needs to empty out existing containers and revert to empty state --> refresh the page
-    location.reload();
-// get JSON data for next story based on value of storyCounter. storyCounter = 0 is the [0] array in the JSON object; the storyCounter grows by 1 each time it is clicked, returning the next value in the object each time.
-    $.getJSON('http://10.90.12.47:5000/articles', function(data) {
-        // depending on what story you are reading, when you click next, it will return the next item
-        // need to pull in article image
-        $('.articleHeadline').html(data.headline[storyCounter]);
-        $('.byline').html(data.author[storyCounter]);
-        $('.articleText').append('<ul>' + data.body[storyCounter] + '</ul>');
-    // add one to storyCounter
-        storyCounter++;
-       });
+// function getBullets() {
 
-}
+//     // $.getJSON('http://10.90.12.47:5000/articles', function(data) {
+//     //     // depending on what story you are reading, when you click next, it will return the next item
+//     //     // need to pull in article image
+//     //     $('.articleHeadline').html(data.headline[storyCounter]);
+//     //     $('.byline').html(data.author[storyCounter]);
+//     //     $('.articleText').append('<ul>' + data.body[storyCounter] + '</ul>');
+//     // add one to storyCounter
+//         storyCounter++;
+//        });
+
+//}
 
 function trashArticle() {
-    // record that they didn't like this article
+    // store that this was an article the person didn't like
+    // return to the article list page?
+    // OR get the next story
+//    getJSON();
 
-    // then get the next article
-    getJSON();
 }
-
-//
-//
-// $("#NAMEHEREHEREHERe").click(function(event){
-//
-// });
