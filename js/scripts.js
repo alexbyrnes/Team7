d0743ebc56e7b8d91f0dc1c51671968fec79e4fa
Melissa Steffan <msteffan2012@gmail.com>
2015-07-12 11:36:34 -0400

    added in links from index and bootcards

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,9 @@ var storyCounter = 0;
 $("#trash").click(trashArticle);
 $("#nextStory").click(getJSON)
 
+$(window).load(function() {
+    getJSON();
+});
 // bring on the json. jquery-->ajax function to pull in article information to article page
 function getJSON(){
    $.ajax({
@@ -14,13 +17,15 @@ function getJSON(){
         success: function(data) {
             // store the parsed json as a variable
             json = $.parseJSON(data);
-            // // get JSON data for next story based on value of storyCounter. storyCounter = 0 is the [0] array in the JSON object; the storyCounter grows by 1 each time it is clicked, returning the next value in the object each time.
+            // get data for next story based on value of storyCounter. storyCounter = 0 is the [0] array in the JSON object; the storyCounter grows by 1 each time it is clicked, returning the next value in the object each time.
             $('.byline').html("By " + json[storyCounter].author);
             $('.articleHeadline').html(json[storyCounter].headline);
             $('.articleText').html(json[storyCounter].copy);
+            // change value of story counter
+            storyCounter++;
             }
         });
-    storyCounter++;
+
 }
 
 // function getBullets() {
@@ -39,8 +44,7 @@ function getJSON(){
 
 function trashArticle() {
     // store that this was an article the person didn't like
-    // return to the article list page?
-    // OR get the next story
-//    getJSON();
+    // get the next story
+    getJSON();
 
 }
